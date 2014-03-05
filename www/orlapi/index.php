f3a704c76d2ebff9125947d3e13a57c9f0d2e71e
ajpowell <adrian.powell@apmf.co.uk>
2014-03-05 08:48:40 +0000

    Added flight (REST library) to orlapi

---
+++ www/orlapi/index.php
@@ -0,0 +1,65 @@
+<?php
+require 'flight/Flight.php';
+
+Flight::route('/', function(){
+    echo '<h2>Flight</h2><br>hello world!';
+});
+
+Flight::route('/too', function(){
+    echo '<h2>Flight</h2><br>hello world too!';
+});
+
+Flight::route('/level/@siteid/latest', function($siteid){
+    if (!$link = mysql_connect('localhost', 'apmfcouk_river1', '12341qaz')) {
+      echo 'Could not connect to mysql';
+      exit;
+    }
+
+    if (!mysql_select_db('apmfcouk_riverlevels', $link)) {
+        echo 'Could not select database';
+        exit;
+    }
+
+    $sql    = 'SELECT sitename, level, date_format(lev.read_dt, \'%H:%i %e/%m/%Y\') last_read FROM levels lev join sites sit on sit.siteid = lev.siteid WHERE lev.siteid = "' . $siteid . '" order by read_dt desc limit 1;';
+    $result = mysql_query($sql, $link);
+
+    if (!$result) {
+        echo "DB Error, could not query the database\n";
+        echo 'MySQL Error: ' . mysql_error();
+        exit;
+    }
+
+    while ($row = mysql_fetch_assoc($result)) {
+        #echo $row['level'];
+        return Flight::json(array('siteid' => $siteid,
+                                  'level' => $row['level'],
+                                  'sitename' => $row['sitename'],
+                                  'read_dt' => $row['last_read']));
+    }
+
+    mysql_free_result($result);
+});
+
+Flight::route('/template', function(){
+    #echo '<h2>Flight</h2><br>hello world too!';
+
+    Flight::view()->set('name', 'Bob');
+
+    Flight::render('templatetest');
+});
+
+Flight::route('/json/', function(){
+    return Flight::json(array('siteid' => 7051,
+                              'sitename' => 'Shipton',
+                              'watercourse' => 'Evenlode',
+                              'level' => 1.42));
+});
+
+# Catch anything not matched...
+Flight::route('*', function($route){
+    echo '<h1>Flight</h1><br><h2>404 Error - ' . $_SERVER['REQUEST_URI'] . ' - Page not found</h2>';
+});
+
+
+Flight::start();
+?>
